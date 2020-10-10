d6cee78f92b7881175c8992c80adbc9f40395113
syntaxiom <seanlesterwilson@gmail.com>
2020-10-09 17:41:41 -0700

    Full size container, get! 🎊

---
+++ lib/design/full_size_container.dart
@@ -0,0 +1,48 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+enum Dimension {
+  width,
+  height,
+  all,
+}
+
+class FullSizeContainer extends StatefulWidget {
+  final Dimension dimension;
+  final Widget child;
+  
+  FullSizeContainer({this.dimension = Dimension.width, this.child});
+  
+  @override
+  _FullSizeContainerState createState() => _FullSizeContainerState();
+}
+
+class _FullSizeContainerState extends State<FullSizeContainer> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    double width;
+    double height;
+    
+    switch (widget.dimension) {
+      case Dimension.width:
+        width = screenSize.width;
+        break;
+      case Dimension.height:
+        height = screenSize.height;
+        break;
+      case Dimension.all:
+      default:
+        width = screenSize.width;
+        height = screenSize.height;
+        break;
+    }
+    
+    return Container(
+      width: width,
+      height: height,
+      child: widget.child,
+    );
+  }
+}
--- lib/pages/home_page/panel_items/blood_stats.dart
+++ lib/pages/home_page/panel_items/blood_stats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:monstr_app/design/full_size_container.dart';
 import 'package:monstr_app/models/blood.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -15,16 +16,15 @@ class _BloodStatsState extends State<BloodStats> {
   @override
   Widget build(BuildContext context) {
     Blood blood = widget.blood;
-    var screenSize = MediaQuery.of(context).size;
     
-    return Container(
-      width: screenSize.width,
+    return FullSizeContainer(
+      dimension: Dimension.width,
       child: LinearPercentIndicator(
         width: 150,
         lineHeight: 20,
         progressColor: Colors.red,
         percent: blood.currentAmount / blood.maxAmount,
-      )
+      ),
     );
   }
 }
