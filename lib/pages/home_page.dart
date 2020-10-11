e84aeb6a7870bcd43f8053a7b5c3848dfeac2562
syntaxiom <seanlesterwilson@gmail.com>
2020-10-11 16:24:10 -0700

    Just when I thought I had BLoC figured out...I improve! 🎊

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,48 +7,39 @@ import 'package:monstr_app/components/blood_stats.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<BloodListBloc, BloodListState>(
-      builder: (context, state) {
-        switch (state.runtimeType) {
-          case LoadSuccessState:
-            // SURVIVAL
-            var bloodList = (state as LoadSuccessState).bloodList.toList();
-            var chosenBloodList = bloodList.where((blood) => blood.chosen).toList();
-            var bloodStatsList = chosenBloodList.map((bloodElement) => BloodStats(blood: bloodElement)).toList();
-            
-            return Scaffold(
-              body: Center(
-                child: Column(
-                  // TODO: Figure out if alignment supercedes the need for a Center component
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <PanelList>[
-                    // TODO: Panel data array
-                    PanelList(
-                      route: '/cosmos',
-                      color: Colors.purple,
-                      // items: bloodStatsList,
-                    ),
-                    PanelList(
-                      route: '/love',
-                      color: Colors.green,
-                      // items: bloodStatsList,
-                    ),
-                    PanelList(
-                      // TODO: Make the title dynamic for different food/transform types?
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            PanelList(
+              route: '/cosmos',
+              color: Colors.purple,
+              // items: bloodStatsList,
+            ),
+            PanelList(
+              route: '/love',
+              color: Colors.green,
+              // items: bloodStatsList,
+            ),
+            BlocBuilder<BloodListBloc, BloodListState>(
+              builder: (context, state) {
+                switch (state.runtimeType) {
+                  case LoadSuccessState:
+                    var bloodList = (state as LoadSuccessState).bloodList.toList();
+                    var chosenBloodList = bloodList.where((blood) => blood.chosen).toList();
+                    var bloodStatsList = chosenBloodList.map((bloodElement) => BloodStats(blood: bloodElement)).toList();
+                      
+                    return PanelList(
                       route: '/survival',
                       color: Colors.orange,
                       items: bloodStatsList,
-                    ),
-                  ],
-                ),
-              ),
-            );
-          default:
-            // TODO: Load the bloodList properly for production purposes
-            BlocProvider.of<BloodListBloc>(context).add(LoadSuccessEvent());
-            return Container();
-        }
-      },
+                    );
+                }
+              }
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/survival_page.dart
+++ lib/pages/survival_page.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:monstr_app/components/blood_group.dart';
-import 'package:monstr_app/data/blood_list.dart';
-// import 'package:monstr_app/design/checkbox_input.dart';
-// import 'package:monstr_app/design/full_size_container.dart';
-// import 'package:monstr_app/models/blood.dart';
-// import 'package:monstr_app/components/blood_stats.dart';
 
 class SurvivalPage extends StatefulWidget {
+  
+  
   @override
   _SurvivalPageState createState() => _SurvivalPageState();
 }
@@ -15,12 +11,14 @@ class SurvivalPage extends StatefulWidget {
 class _SurvivalPageState extends State<SurvivalPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement a `FilteredBloodList` bloc for scalability
-    
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(color: Colors.orange),
-        child: BloodGroup()
+        child: Column(
+          children: [
+            BloodGroup(),
+          ],
+        )
       ),
     );
   }
